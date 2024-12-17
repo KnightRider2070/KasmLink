f1a32f2a0520a5739215865b84e17234d255acd8
Christoph <70174704+KnightRider2070@users.noreply.github.com>
2024-12-17 20:28:21 +0100

    Add workspace endpoint

--- Tests/variableFile.go
+++ Tests/variableFile.go
@@ -1,5 +1,7 @@
 package Tests
 
+var alternateValues = true
+
 var baseUrl = "https://192.168.56.103"
 var apiSecret = "rBmnE9mHLmzd"
 var apiKeySecret = "8A1xrZLhm14WFuk9Gf9AerVMljdPzIhk"
@@ -7,6 +9,27 @@ var hostIp = "192.168.56.103"
 var user = "thor"
 var password = "stark"
 var knwHosts = "C:\\Users\\cjhue\\.ssh\\known_hosts"
+
+// Alternate Values
+var altBaseUrl = "https://192.168.120.5"
+var altApiSecret = "QpuCkIfRl2Mj"
+var altApiKeySecret = "fsd6xERiqRXoFTAPXhiRSX20s4gBdiwN"
+var altHostIp = "192.168.120.5"
+var altUser = "thor"
+var altPassword = "stark"
+var altKnwHosts = "C:\\Users\\thor\\.ssh\\known_hosts"
+
+func init() {
+	if alternateValues {
+		baseUrl = altBaseUrl
+		apiSecret = altApiSecret
+		apiKeySecret = altApiKeySecret
+		hostIp = altHostIp
+		user = altUser
+		password = altPassword
+		knwHosts = altKnwHosts
+	}
+}
 
 //Create ssh config
 //sshConfig, _ := sshmanager.NewSSHConfig(user, password, hostIp, 22, knwHosts, 10*time.Second)
